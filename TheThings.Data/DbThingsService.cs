f972e77016922df9bc3fd254ee318713dc1ed302
mjack31 <shark3113@gmail.com>
2019-04-08 23:42:38 +0200

    Add db connection and data service implementation

---
+++ TheThings.Data/DbThingsService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheThings.Data.Interfaces;
+using TheThings.Models;
+using System.Linq;
+
+namespace TheThings.Data
+{
+    class DbThingsService : IThingsRepository
+    {
+        private readonly ThingsDbContext thingsDbContext;
+
+        public DbThingsService(ThingsDbContext thingsDbContext)
+        {
+            this.thingsDbContext = thingsDbContext;
+        }
+
+        public Thing Add(Thing thingToAdd)
+        {
+            thingsDbContext.Things.Add(thingToAdd);
+            return thingToAdd;
+        }
+
+        public int CountThings()
+        {
+            return thingsDbContext.SaveChanges();
+        }
+
+        public Thing Delete(int idToDelete)
+        {
+            var thing = GetById(idToDelete);
+            if(thing != null)
+            {
+                thingsDbContext.Things.Remove(thing);
+            }
+            return thing;
+        }
+
+        public Thing GetById(int id)
+        {
+            return thingsDbContext.Things.FirstOrDefault();
+        }
+
+        public IEnumerable<Thing> GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Thing Update(Thing updatedRestaurent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TheThings.Data/ThingsDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheThings.Models;
+
+namespace TheThings.Data
+
+// Aby mieć pewność ze jest dobrze skonfigurowane połączenie z bazą danych należ pozyskać info o DbContext przez Entity Framework Core CLI
+// Komenda w projekcie danych (tutaj TheThings.Data) dotnet ef dbcontext info
+// Aby sprawdzić czy w ogóle jest dobrze zinicjalizowany DbContext można użyć komendy dotnet ef dbcontext info. To pokaże dostępne DbContext'y
+// Na końcu komendy info trzeba zdefiniować projekt startowy w którym zazwyczaj jest konfiguracja połączenia - connection string (appsettings.json) oraz ustawienia (Startup.cs)
+// Robi się to dodając na końcu -s i lokalizację projektu z konfiguracją (tutaj -s ../TheThings_FirstAspApp/TheThings_FirstAspApp.csproj)
+
+{
+    public class ThingsDbContext : DbContext
+    {
+        public ThingsDbContext(DbContextOptions<ThingsDbContext> opt)
+            : base(opt)
+        {
+
+        }
+
+        public DbSet<Thing> Things { get; set; }
+    }
+}
